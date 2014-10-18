649d41c43c8de1b6ab70b5b2d6209af9e35e0a34
Claudio Reyes Miranda <claudio.reyes.miranda@gmail.com>
2014-10-17 23:25:49 -0300

    Incorpora motor indexhibit

--- Engine/index.php
+++ Engine/index.php
@@ -1,77 +1,58 @@
-<?php include("seguridad.php");?>
-<!DOCTYPE HTML>
+<?php define('SITE', 'Bonjour!');
 
-<html>
-<link rel="shortcut icon" href="http:favicon.ico">
-	<style type="text/css">
-<!--
-.Estilo2 {color: #FFFFFF}
--->
-    </style>
-	<head>
-		<title>GESTEL</title>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<meta name="description" content="" />
-		<meta name="keywords" content="" />
-		<link href="http://fonts.googleapis.com/css?family=Oswald:400,300" rel="stylesheet" type="text/css" />
-		<!--[if lte IE 8]>
-        <script src="Interno/RegistroClienteProveedor/js/html5shiv.js"></script><![endif]-->
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-		<script src="Interno/RegistroClienteProveedor/js/skel.min.js"></script>
-		<script src="Interno/RegistroClienteProveedor/js/skel-panels.min.js"></script>
-		<script src="Interno/RegistroClienteProveedor/js/init.js"></script>
-		<noscript>
-			<link rel="stylesheet" href="Interno/RegistroClienteProveedor/css/skel-noscript.css" />
-			<link rel="stylesheet" href="Interno/RegistroClienteProveedor/css/style.css" />
-			<link rel="stylesheet" href="Interno/RegistroClienteProveedor/css/style-desktop.css" />
-		</noscript>
-	</head>
-	
-	<body class="homepage">
-		<div id="header-wrapper">
-			<div class="container">
-				<div id="header">				  
-				   <?php include("menu.php"); ?>					
-				</div> <!--Div header -->
-				<div align="center">
-					<?php include("login.php"); ?>				  
-			</div> <!-- div container -->
-		</div> <!-- div header-wrapper --> 
-		<div class="container">
-			<div class="row">
-				<div id="banner" class="12u">
-					<div class="container"><a href="#"><img src="Interno/RegistroClienteProveedor/images/pics01.jpg" alt="GESTEL" alt="270" height="325"> </a></div>
-				</div>
-			</div>
-		</div><!--container -->
-		<div id="wrapper">
-		<div class="container" id="marketing">
-				<div class="row divider">
-					<div class="3u">
-						<section>
-							<p><a href="#"><img src="Interno/RegistroClienteProveedor/images/ico04.png" alt=""></a></p>
-						</section>
-					</div>
-					<div class="3u">
-						<section>
-							<p><a href="#"><img src="Interno/RegistroClienteProveedor/images/ico05.png" alt=""></a></p>
-						</section>
-					</div>
-					<div class="3u">
-						<section>
-							<p><a href="#"><img src="Interno/RegistroClienteProveedor/images/ico06.png" alt=""></a></p>
-						</section>
-					</div>
-					<div class="3u">
-						<section>
-							<p><a href="#"><img src="Interno/RegistroClienteProveedor/images/ico07.png" alt=""></a></p>
-						</section>
-					</div>
-				</div><!-- div row divider -->
-			</div><!-- div marketing -->
-		</div><!-- div wrapper -->
-		<?php include("container.php"); ?>
-		</div>
-		<?php include("footer.php"); ?>
-</body>
-</html>
+// -----------------------------------------------------------
+// 	WELCOME TO INDEXHIBIT
+//
+//  A collaboration between Daniel Eatock and Tatiret, c.o.
+//  Open source and free to use for good in this world.
+//
+//  If we've missed a credit, or you simply deserve more
+//  credit today, just let us know. Thank you!!!
+//
+//  An MVC without the V. Or with a small v. ;)
+//
+//  Thank you for shopping at indexhibit.org
+// -----------------------------------------------------------
+
+// turn this on if you want to check things
+//error_reporting(E_ALL);
+
+// the basics
+if (file_exists('config/config.php')) require_once 'config/config.php';
+
+require_once 'defaults.php';
+require_once 'common.php';
+
+// make sure we have our connection array
+shutDownCheck();
+
+// preloading things
+load_helpers(array('html', 'entrance', 'time', 'server'));
+
+// general tools for loading things
+load_class('core', FALSE, 'lib');
+
+// "I'm digging for fire" - Pixies	
+$OBJ =& load_class('router', TRUE, 'lib');
+
+// are we logged in?
+$OBJ->access->checkLogin();
+
+// get user prefernces
+$OBJ->lang->setlang($OBJ->access->prefs['user_lang']);
+
+// loading our module object
+$INDX =& load_class($go['a'], TRUE, 'mod', TRUE);
+
+// referencing wonkiness
+// review when there is time
+$aINDX =& $INDX;
+
+// loading our module method
+$OBJ->tunnel($aINDX, $go['a'], $go['q']);
+
+// output
+$INDX->template->output('index');
+
+
+?>
--- index.php
+++ index.php
@@ -1,13 +1,102 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: inidentificable
- * Date: 25-08-14
- * Time: 03:36 AM
- */
-if (isset($_POST["autorizado"])) {
-    echo "Bienvenido, aun no hay nada por aqui";
-}else{
-    echo "Tu no puedes estar aqui";
+//Este archivo está basado en el CMS de indexhibit 1.1
+
+<?php define('SITE', 'Bonjour!');
+
+// turn this on if you want to check things
+//error_reporting(E_ALL);
+
+// the basics
+if (file_exists('Engine/config/config.php')) require_once 'Engine/config/config.php';
+else {
+    header('location: Engine/install.php');
 }
+
+
+require_once 'Engine/defaults.php';
+require_once 'Engine/common.php';
+
+// make sure we have our connection array
+shutDownCheck();
+
+// messy, but seems to work
+$_REAL_SCRIPT_DIR = realpath(dirname($_SERVER['SCRIPT_FILENAME']));
+$_REAL_BASE_DIR = realpath(dirname(__FILE__));
+$_MY_PATH_PART = substr($_REAL_SCRIPT_DIR, strlen($_REAL_BASE_DIR));
+$uri = $_MY_PATH_PART
+    ? substr(dirname($_SERVER['SCRIPT_NAME']), 0, -strlen($_MY_PATH_PART))
+    : dirname($_SERVER['SCRIPT_NAME']);
+
+// clean up the uri
+$uri = (MODREWRITE == false) ?
+    str_replace('/index.php?', '', $_SERVER['REQUEST_URI']) :
+    entry_uri($uri, $_SERVER['REQUEST_URI']);
+
+// TEMPORARY
+// what if it's more than one folder up?
+$uri = str_replace($self, '', $uri);
+
+//remove out the third level of url since it doesn't work.
+if (count($uri_arr = explode('/', $uri)) > 3) {
+    $uri = '/' . $uri_arr[1] . '/' . $uri_arr[2] . '/';
+}
+
+// time to roll
+load_helpers(array('time'));
+$OBJ =& load_class('core', TRUE, 'lib');
+
+// page query
+$rs = $OBJ->db->fetchRecord("SELECT * 
+	FROM " . PX . "objects, " . PX . "objects_prefs
+	WHERE url = '$uri'
+	AND status = '1' 
+	AND object = obj_ref_type");
+
+if (!$rs) {
+    // try again with site root
+    $rs = $OBJ->db->fetchRecord("SELECT *
+		FROM " . PX . "objects, " . PX . "objects_prefs
+		WHERE id = 3
+		AND status = '1' 
+		AND object = obj_ref_type");
+
+    // we don't search engines indexing this
+    header("HTTP/1.1 404 Not Found");
+
+    // we need a formal error page
+    if (!$rs) echo 'Page not found error here.';
+}
+// autoload 'plugins' folder
+include DIRNAME . BASENAME . '/site/plugin/index.php';
+
+// additional variables
+// perhaps we should port these differently?
+$rs['baseurl'] = BASEURL;
+$rs['basename'] = BASENAME;
+$rs['basefiles'] = BASEFILES;
+$rs['gimgs'] = GIMGS;
+
+// get the front end helper class
+$OBJ->lib_class('front');
+
+// time for some action
+if ($rs['obj_theme'] == 'eatock') {
+    $contents = $OBJ->front->front_eatock();
+} else {
+    $filename = DIRNAME . BASENAME . '/site/' . $rs['obj_theme'] . '/index.php';
+    $fp = @fopen($filename, 'r');
+    $contents = fread($fp, filesize($filename));
+    fclose($fp);
+}
+
+// makin' stuff happen
+$PARSE =& load_class('parse', TRUE, 'lib');
+$PARSE->vars = $rs;
+$PARSE->code = $contents;
+echo $PARSE->parsing();
+
+// do stats if they are wanted
+if ($default['statistics'] == TRUE) load_class('statistics', TRUE, 'lib');
+exit;
+
+
 ?>
