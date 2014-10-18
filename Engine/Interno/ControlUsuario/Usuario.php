864a5a9e0d941b0db4156c5a8778b20bfaee0f2e
Claudio Reyes Miranda <claudio.reyes.miranda@gmail.com>
2014-10-17 22:39:08 -0300

    agrega modificaciones de Maria Fernanda 2

--- Engine/Interno/ControlUsuario/Usuario.php
+++ Engine/Interno/ControlUsuario/Usuario.php
@@ -7,9 +7,11 @@
  */
 
 namespace ControlUsuario;
+include_once("");
 
 
-class Usuario {
+class Usuario
+{
     // Declaración de la propiedad
     public $nombreUsuario;
     private $idEsAdministrador;
@@ -17,28 +19,62 @@ class Usuario {
     private $emailUsuario;
 
     // Declaración del método
-    public function CrearUsuario() {
+    public function CrearUsuario()
+    {
+        public
+        Usuario $UsuarioNuevo;
 
     }
-    public function EliminarUsuario() {
+
+    public function EliminarUsuario()
+    {
 
     }
-    public function ModificarUsuario() {
+
+    public function ModificarUsuario()
+    {
 
     }
-    public function ListarUsuario() {
+
+    public function ListarUsuario()
+    {
 
     }
-    public function ModificarClaveUsuario() {
+
+    public function ModificarClaveUsuario()
+    {
 
     }
-    public function ModificarEmailUsuario() {
+
+    public function ModificarEmailUsuario()
+    {
 
     }
-    public function AsignarAdministrador() {
+
+    public function AsignarAdministrador()
+    {
 
     }
-    public function EliminarAdministrador() {
+
+    public function EliminarAdministrador()
+    {
 
     }
+
+    function usuarioAsociado($cod)
+    {
+        if (!empty($cod)) {
+            conecta_base();
+            $sql2 = "SELECT nombre_user, apellido_user FROM usuario WHERE activo_user = 1 AND id_usuario = " . $cod;
+            $res2 = mysql_query($sql2);
+            if (mysql_num_rows($res2) == 1) {
+                $dato = mysql_fetch_array($res2);
+                $html = $dato[0] . " " . $dato[1];
+            } else {
+                $html = "Sin usuario";
+            }
+            return $html;
+        }
+    }
+
 } 
