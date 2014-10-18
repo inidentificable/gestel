507191f1d1255a806fe0530708edcd6b07df4a39
Claudio Reyes Miranda <claudio.reyes.miranda@gmail.com>
2014-10-17 22:39:08 -0300

    agrega modificaciones de Maria Fernanda 2

---
+++ Engine/Interno/ControlDatos/Multimedia.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inidentificable
+ * Date: 11-10-14
+ * Time: 03:07 PM
+ */
+
+namespace ControlDatos;
+
+
+class Multimedia
+{
+    function imagen($archivo)
+    {
+        $ruta = "../imagenes/" . $archivo;
+        if (file_exists($ruta)) {
+            $t = getimagesize($ruta);
+            return '<img src="' . $ruta . '" border="0" width="' . $t[0] . '" height="' . $t[1] . '" alt="" style="vertical-align: middle;" />';
+        }
+        return '';
+
+    }
+
+    function espacio($ancho, $alto)
+    {
+        $ruta = "../imagenes/espacio.png";
+        if (file_exists($ruta)) {
+            return '<img src="' . $ruta . '" border="0" width="' . $ancho . '" height="' . $alto . '" alt="" />';
+        }
+        return '';
+    }
+} 
